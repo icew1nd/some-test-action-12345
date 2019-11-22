ab87c5dc1bc4c53448ebf978f25e2990cfd14b2b
Thomas Kjær-Rasmussen <thomaskjaer-rasmussen@Thomass-MacBook-Pro.local>
2019-11-22 10:28:04 +0100

    updated

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const request = require("request");
+const fs = require("fs");
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
+  const url = core.getInput("who-to-greet");
+  if (!url) {
+    throw new Error("No URL specified");
+  }
+  console.log(`Posted screenshot to GitHub PR`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+
+  // const options = {
+  //   method: "POST",
+  //   url:
+  //     "https://api.saasify.sh/1/call/transitive-bullshit/puppet-master@a818aeac/screenshot",
+  //   headers: {
+  //     "content-type": "application/json"
+  //   },
+  //   body: {
+  //     url
+  //   },
+  //   json: true
+  // };
+
+  // request(options, (error, response, body) => {
+  //   if (error) throw new Error(error);
+
+  const context = github.context;
+
+  const newIssue = await octokit.issues.create({
+    ...context.repo,
+    title: "New issue!",
+    body: "Hello Universe!"
+  });
+
+  console.log(JSON.stringify(context));
+  octokit.pulls.createReview({
+    owner: context.repo.owner,
+    repo: context.repo,
+    pull_number: context.pull_number,
+    body: "yihaa"
+  });
+  // });
 } catch (error) {
   core.setFailed(error.message);
 }
