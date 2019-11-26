bdb8340e9ad23a7fcb6e51f40e6cf1e94045e1e8
Thomas Kjær-Rasmussen <thomaskjaer-rasmussen@thomass-mbp.lan>
2019-11-26 17:45:15 +0100

    lol

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const request = require("request");
-const fs = require("fs");
 const Octokit = require("@octokit/rest");
 const auth = core.getInput("gh-token");
 const octokit = new Octokit({ auth });
 
 try {
   const payload = github.context.payload;
-  console.log(payload);
-  const owner = payload.repository.owner.id;
+  const owner = payload.repository.owner.login;
   const repo = payload.repository.name;
   const pull_number = payload.number;
 
-  console.log("✔️");
-  octokit.pulls.createReview({
-    owner,
-    repo,
-    pull_number,
-    body: "❤️"
-  });
+  octokit.pulls
+    .createReview({
+      owner,
+      repo,
+      pull_number,
+      body: "❤️"
+    })
+    .then(() => {
+      console.log("✔️");
+    })
+    .catch(error => {
+      throw new Error(error);
+    });
 } catch (error) {
+  console.log(auth);
   core.setFailed(error.message);
 }
